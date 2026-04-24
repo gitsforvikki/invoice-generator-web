e5b417408a2ee817f7aee1da7ec72dafc2adec68
Vikash <vk6484412@gmail.com>
2026-04-24 10:27:39 +0530

    initial setup for frontend done

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import InvoiceForm from '@/components/InvoiceForm';
+
+export default function Home() {
+  return (
+    <div className="py-10 px-4 sm:px-6 lg:px-8">
+      <InvoiceForm />
+    </div>
+  );
+}
